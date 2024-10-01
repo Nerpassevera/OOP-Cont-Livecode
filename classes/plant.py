287009ff7de69d3efd9379f02dcacb934432cd65
Tatiana Trofimova <nerpassevera@gmail.com>
2024-10-01 11:08:52 -0700

    My solution

--- classes/plant.py
+++ classes/plant.py
@@ -1,4 +1,30 @@
 class Plant:
 
-# Write your plant class here!
-    pass
+    def __init__(self, name, water_lavel, sunlight_hours, is_blooming=False):
+        self.name = name
+        self.water_level = water_lavel
+        self.sunlight_hours = sunlight_hours
+        self.is_blooming = is_blooming
+        self.growth_stage = self.check_growth()
+
+    def water(self, water_amount):
+        self.water_level += water_amount
+        self.check_growth()
+
+    def give_sunlight(self, sunlight_hours_to_add):
+        self.sunlight_hours += sunlight_hours_to_add
+        self.check_growth()
+
+    def check_growth(self):
+        if self.water_level > 5 and self.sunlight_hours > 5:
+            self.growth_stage = "Mature" 
+        else:
+            self.growth_stage = "Sprout"
+        return self.growth_stage
+    
+    def __repr__(self):
+        return f"A {self.name} flower with {self.water_level} water level, {self.sunlight_hours} sunlight hours which is {"not" if not self.is_blooming else None} in bloom"
+
+plant = Plant("Orchid", 3, 2)
+print(plant)
+print(plant.growth_stage)
--- tests/test_plant.py
+++ tests/test_plant.py
@@ -1,1 +1,27 @@
 from classes.plant import Plant
+
+def test_plant_class_creates_instance_with_attributes():
+    plant_name = "Butterfly pea flower"
+    plant_water_level = 6
+    plant_sunlight_hours = 7
+    plant_is_blooming = True
+
+    plant = Plant(plant_name, plant_water_level, plant_sunlight_hours, plant_is_blooming)
+
+    assert plant.name == plant_name
+    assert plant.water_level == plant_water_level
+    assert plant.sunlight_hours == plant_sunlight_hours
+    assert plant.is_blooming == plant_is_blooming
+
+def test_plant_class_updates_growth_stage_after_watering():
+    plant_name = "Butterfly pea flower"
+    plant_water_level = 3
+    plant_sunlight_hours = 6
+    plant_is_blooming = True
+    water_to_add = 3
+
+    plant = Plant(plant_name, plant_water_level, plant_sunlight_hours, plant_is_blooming)
+    plant.water(water_to_add)
+
+
+    assert plant.growth_stage == "Mature"
